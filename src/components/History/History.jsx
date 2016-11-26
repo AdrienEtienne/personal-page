eb7a85e1c579b0278a00cf8090304e0af7d2f7cf
Adrien ETIENNE <aee@cogesys.com>
2016-11-26 16:27:14 -0500

    ajout composant History

---
+++ src/components/History/History.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './History.scss'
+
+export const History = () => (
+    <div className="History">
+        <hr className="Line TopLine"/>
+        <div className="Content">
+            <hr className="Line"/>
+            <h1>Content 1</h1>
+        </div>
+        <hr className="Line BottomLine"/>
+    </div>
+)
+
+export default History
--- src/routes/Home/components/HomeView.jsx
+++ src/routes/Home/components/HomeView.jsx
@@ -1,52 +1,76 @@
 import React from 'react'
 import Javascript from '../../../assets/images/javascript.png'
+import Nodejs from '../../../assets/images/nodejs.png'
+import Angularjs from '../../../assets/images/angularjs.png'
+import Reactjs from '../../../assets/images/reactjs.png'
+import Dotnet from '../../../assets/images/dotnet.png'
 import Bar from '../../../components/Bar'
 import Teckonaut from '../../../components/Teckonaut'
 import ButtonCross from '../../../components/ButtonCross'
 import HoverBand from '../../../components/HoverBand'
+import History from '../../../components/History'
 import './HomeView.scss'
 
 export class HomeView extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state={
-      toggled:true
+    this.state = {
+      toggled: false
     }
   }
 
-  onToggle(){
-    this.setState({toggled:!this.state.toggled})
+  onToggle() {
+    this.setState({
+      toggled: !this.state.toggled
+    })
   }
 
   render() {
     return (
       <div className="HomeView">
-        <h1>
-          Founder of
-          <br></br>
-          <Teckonaut></Teckonaut>
-        </h1>
-        <div>
-          <ButtonCross toggle={this.onToggle.bind(this)} toggled={this.state.toggled}></ButtonCross>
+        <div className="javascript-bar">
+          <ButtonCross
+            toggle={this
+            .onToggle
+            .bind(this)}
+            toggled={this.state.toggled}></ButtonCross>
+          <Bar
+            image_alt="This is Javascript!"
+            image={Javascript}
+            text="In conflictual relationship with Javascript"></Bar>
         </div>
-
-        <Bar
-          image_alt="This is Javascript!"
-          image={Javascript}
-          text="In conflictual relationship with Javascript"></Bar>
-
-        <HoverBand>
-          test de contenue
-        </HoverBand>
+        <div
+          className={'js-bars' + (this.state.toggled
+          ? ''
+          : ' hide')}>
+          <div className="nodejs-bar">
+            <Bar
+              image={Nodejs}
+              text="Follower of NodeJS techs. Listening news, religiously"></Bar>
+          </div>
+          <div className="angularjs-bar">
+            <Bar image={Angularjs} text="Work hard with it"></Bar>
+          </div>
+          <div className="reactjs-bar">
+            <Bar image={Reactjs} text="My new hobby"></Bar>
+          </div>
+        </div>
+        <div className="dotnet-bar">
+          <Bar image={Dotnet} text="A durable friendship between us"></Bar>
+        </div>
         <div className="container">
-          <h4>Welcome! Duck</h4>
+          <History text="Mon histoire 1"></History>
+          <History text="Mon histoire 2 avec contenue">
+            <h1>Mon contenue</h1>
+            <p>Mon paragraphe de contenue</p>
+          </History>
+
         </div>
       </div>
     );
   }
 }
-
 
 HoverBand.propTypes = {};
 HoverBand.defaultProps = {};
